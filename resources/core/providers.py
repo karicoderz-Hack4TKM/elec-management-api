4ed36e548771506cf12faabef8917e2816c70a20
Rahul <rahulrajeev067@gmail.com>
2021-12-05 01:24:05 +0530

    providers added

--- resources/core/providers.py
+++ resources/core/providers.py
@@ -3,10 +3,42 @@ from flask_restful import Resource
 import pymongo
 import common.auth.token_utils as auth
 from datetime import datetime
+import common.auth.encryption_service as cipher
 import uuid
 import json
 
 class Providers(Resource):
+    @auth.verifyToken
+    def post(self, **tokenData):
+        y = tokenData['userDetails']
+        connect = pymongo.MongoClient(current_app.config["MONGO_URL"])
+        selectDb = connect[current_app.config["DB_NAME"]]
+        selectCollection = selectDb["providers"]
+        try:
+            data = request.get_json()
+            mydoc = selectCollection.find_one({"email": data["email"]})
+            # checking whether user is already present
+            if mydoc is None:
+                # encrypting password
+                data["password"] = str(uuid.uuid4().hex)
+                pwd = data["password"]
+
+                data["password"] = cipher.EncryptionService().encryptText(data["password"],
+                                                                          current_app.config["CIPHER_KEY"])
+                # addOn = {"createdOn": datetime.now(), "updatedOn": datetime.now(), "_id": str(uuid.uuid4())}
+                # data.update(addOn)
+                y = selectCollection.insert_one(data)
+                connect.close()
+                if y.inserted_id:
+                    return {"codee": 201, "message": f"{data['usertype']}  is Successfully Created "}, 201
+                else:
+                    return {"code": 211, "message": "User is not inserted : "}, 401
+            else:
+                return {"code": 212, "message": f"{data['email']} already Present"}, 212
+        except Exception as e:
+            return {"code": 210, "message": "Failed to connect to Mongo DB : " + str(e)}, 500
+
+    @auth.verifyToken
     def get(self, **tokenData):
         try:
             connect = pymongo.MongoClient(current_app.config["MONGO_URL"])
