16ea7db2cde5b857a7aa309f193186d6c4cbc6ab
Rahul <rahulrajeev067@gmail.com>
2021-12-05 06:09:25 +0530

    providers login update

--- resources/core/providers.py
+++ resources/core/providers.py
@@ -4,39 +4,51 @@ import pymongo
 import common.auth.token_utils as auth
 from datetime import datetime
 import common.auth.encryption_service as cipher
-import uuid
 import json
+import jwt
+import datetime
 
 class Providers(Resource):
-    @auth.verifyToken
     def post(self, **tokenData):
-        y = tokenData['userDetails']
-        connect = pymongo.MongoClient(current_app.config["MONGO_URL"])
-        selectDb = connect[current_app.config["DB_NAME"]]
-        selectCollection = selectDb["providers"]
-        try:
-            data = request.get_json()
-            mydoc = selectCollection.find_one({"email": data["email"]})
-            # checking whether user is already present
-            if mydoc is None:
-                # encrypting password
-                data["password"] = str(uuid.uuid4().hex)
-                pwd = data["password"]
+        request_data = request.get_json()
+        email = request_data["email"]
+        password = request_data["password"]
 
-                data["password"] = cipher.EncryptionService().encryptText(data["password"],
-                                                                          current_app.config["CIPHER_KEY"])
-                # addOn = {"createdOn": datetime.now(), "updatedOn": datetime.now(), "_id": str(uuid.uuid4())}
-                # data.update(addOn)
-                y = selectCollection.insert_one(data)
-                connect.close()
-                if y.inserted_id:
-                    return {"codee": 201, "message": f"{data['usertype']}  is Successfully Created "}, 201
+        try:
+            connect = pymongo.MongoClient(current_app.config["MONGO_URL"])
+            selectDb = connect[current_app.config["DB_NAME"]]
+            selectCollection = selectDb["providers"]
+            requiredUserData = selectCollection.find_one({"email": email})
+            connect.close()
+
+            if (requiredUserData != None):
+
+                # decrpting password from db
+                try:
+                    decPassword = cipher.EncryptionService().decryptText(requiredUserData["password"],
+                                                                         current_app.config["CIPHER_KEY"])
+                except Exception as e:
+                    return {"code": "213", "message": "Password Decryption Failure " + str(e)}
+
+                if decPassword == password:
+                    data = requiredUserData
+                    pr = {
+                        "_id": data["_id"],
+                        "email": data["email"],
+                        "password": data["password"],
+                        "usertype": data["usertype"]
+                    }
+
+                    token = jwt.encode({"data": pr, 'exp': datetime.datetime.utcnow() + datetime.timedelta(
+                        minutes=current_app.config["TOKEN_EXPIRY"])}, current_app.config["SECRET_KEY"])
+                    return {"code": "200", "message": "Congrats! Access granted", "token": token, "data": pr}
                 else:
-                    return {"code": 211, "message": "User is not inserted : "}, 401
+                    return {"code": "211", "message": "Incorrect Password!"}
             else:
-                return {"code": 212, "message": f"{data['email']} already Present"}, 212
+                return {"code": "212", "message": "User not found!"}
+
         except Exception as e:
-            return {"code": 210, "message": "Failed to connect to Mongo DB : " + str(e)}, 500
+            return {"code": "210", "message": "Failed to connect to Mongo DB : " + str(e)}
 
     @auth.verifyToken
     def get(self, **tokenData):
